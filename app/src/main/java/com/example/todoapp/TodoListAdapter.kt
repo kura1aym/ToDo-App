c189084ca8cdfac36f4610c915605b41c9ae4a2e
kura1aym <mukhtarkuralay249@mail.ru>
2024-02-24 05:51:05 +0600

    add adapter class

---
+++ app/src/main/java/com/example/todoapp/TodoListAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.todoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.TodoItem
+import com.example.todoapp.R
+
+class TodoItemsAdapter(private val todoItems: List<TodoItem>) :
+    RecyclerView.Adapter<TodoItemsAdapter.TodoItemViewHolder>() {
+
+    inner class TodoItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val checkbox: CheckBox = itemView.findViewById(R.id.checkbox)
+        val textTask: TextView = itemView.findViewById(R.id.textTask)
+        val imageButton: ImageButton = itemView.findViewById(R.id.infoButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoItemViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.task_preview, parent, false)
+        return TodoItemViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TodoItemViewHolder, position: Int) {
+        val currentItem = todoItems[position]
+
+        holder.checkbox.isChecked = currentItem.isDone
+        holder.textTask.text = currentItem.description
+
+
+        holder.imageButton.setOnClickListener {
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return todoItems.size
+    }
+}
